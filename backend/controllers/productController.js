5492c4ef0b0ea081e1397d1d2a81f9ecd671466f
sirichandana-v <88175932+sirichandana-v@users.noreply.github.com>
2022-02-14 22:50:49 +0530

    search filter and pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product=require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
-const catchAsyncErrors=require('../middleware/catchAsyncErrors')
+const catchAsyncErrors=require('../middleware/catchAsyncErrors');
+const ApiFeatures = require("../utils/apiFeatures");
 
 
 
@@ -24,11 +25,16 @@ exports.createProduct= catchAsyncErrors(
 exports.getAllProducts=catchAsyncErrors(
     async(req,res)=>{
 
-        const products=await Product.find();
+        const resultPerPage=5;
+        const productCount=await Product.countDocuments();
+
+        const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+        const products=await apiFeature.query;
     
         res.status(200).json({
             success:true,
-            products
+            products,
+            productCount
         });
     }
 );
@@ -48,7 +54,7 @@ exports.getProductDetails=catchAsyncErrors(
         await product.remove();
         res.status(200).json({
             success:true,
-            product
+            product,
         });
     
     }
