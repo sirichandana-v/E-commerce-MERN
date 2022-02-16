c22d0c139638bcf5a6eca290c3d2db80cb094473
sirichandana-v <88175932+sirichandana-v@users.noreply.github.com>
2022-02-16 20:23:58 +0530

    order api

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ app.use(cookieParser());
 
 const productRoute=require('./routes/productRoute');
 const userRoute=require("./routes/userRoute");
+const orderRoute=require("./routes/orderRoute");
 
 app.use('/api/v1', productRoute);
 app.use('/api/v1', userRoute);
+app.use('/api/v1', orderRoute);
 
 //middleware error
 
